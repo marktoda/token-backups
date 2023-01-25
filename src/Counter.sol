731128ea421c6e6c5bf438adc940c344a4d7fa12
Sara Reynolds <snreynolds2506@gmail.com>
2023-01-25 15:20:52 -0500

    cleanup

--- src/Counter.sol
+++ src/Counter.sol
@@ -7,49 +7,30 @@ import {WitnessLib, Witness} from "./WitnessLib.sol";
 contract TokenBackups {
     using WitnessLib for Witness;
 
-    struct TokenPermissions {
-        // ERC20 token address
-        address token;
-        // the maximum amount that can be spent
-        uint256 amount;
-    }
-
     struct RecoverySigs {
         address newAddress;
         ISignatureTransfer.SignatureTransferDetails[] transferDetails;
     }
 
-    ISignatureTransfer private immutable permit2;
+    ISignatureTransfer private immutable _PERMIT2;
 
-    constructor(address p2) {
-        permit2 = ISignatureTransfer(p2);
+    constructor(address permit2) {
+        _PERMIT2 = ISignatureTransfer(permit2);
     }
 
-    // nonce scheme dont think so?
     function recover(
         bytes calldata recoverySigs,
         bytes calldata setUpSig,
         ISignatureTransfer.PermitBatchTransferFrom calldata permit,
         RecoverySigs calldata recoverySigDetails,
         Witness calldata witnessData,
-        string calldata witnessTypeString,
-        address oldAddress,
-        address newAddress,
-        uint256 nonce
+        address oldAddress
     ) public {
-        // loop through token balances
-        // fetch token balances
-
         bytes32 witness = witnessData.hash();
 
         // owner is the old account address
-        permit2.permitWitnessTransferFrom(
-            permit, recoverySigDetails.transferDetails, oldAddress, witness, witnessTypeString, setUpSig
+        _PERMIT2.permitWitnessTransferFrom(
+            permit, recoverySigDetails.transferDetails, oldAddress, witness, WitnessLib.PERMIT2_WITNESS_TYPE, setUpSig
         );
-
-        // call permit2 witness transfer from
-
-        // loop through the recoverySigs
-        // ecrecover to verify against witness signers list
     }
 }
