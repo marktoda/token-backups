4b5c32cf8e12e3756b2bdc10d878798c8f4861e2
Mark Toda <toda.mark@gmail.com>
2023-01-25 12:44:35 -0800

    feat: add signature verification

--- src/Counter.sol
+++ src/Counter.sol
@@ -2,15 +2,20 @@
 pragma solidity ^0.8.13;
 
 import {ISignatureTransfer} from "permit2/src/interfaces/ISignatureTransfer.sol";
+import {ECDSA} from "openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
 import {WitnessLib, Witness} from "./WitnessLib.sol";
+import {RecoverySigs, RecoverySigsLib} from "./RecoverySigsLib.sol";
 
 contract TokenBackups {
     using WitnessLib for Witness;
+    using RecoverySigsLib for RecoverySigs;
 
-    struct RecoverySigs {
-        address newAddress;
-        ISignatureTransfer.SignatureTransferDetails[] transferDetails;
-    }
+    error NotEnoughSignatures();
+    error InvalidThreshold();
+    error InvalidNewAddress();
+    error InvalidSigner();
+    error NotSorted();
+
 
     ISignatureTransfer private immutable _PERMIT2;
 
@@ -19,18 +24,68 @@ contract TokenBackups {
     }
 
     function recover(
-        bytes calldata recoverySigs,
+        bytes[] calldata recoverySigs,
         bytes calldata setUpSig,
         ISignatureTransfer.PermitBatchTransferFrom calldata permit,
         RecoverySigs calldata recoverySigDetails,
         Witness calldata witnessData,
         address oldAddress
     ) public {
+        if (newAddress == oldAddress) {
+            revert InvalidNewAddress();
+        }
+
+        // loop through token balances
+        // fetch token balances
+
+        _verifySignatures(recoverySigs, recoverySigDetails, witnessData);
+
         bytes32 witness = witnessData.hash();
 
         // owner is the old account address
         _PERMIT2.permitWitnessTransferFrom(
             permit, recoverySigDetails.transferDetails, oldAddress, witness, WitnessLib.PERMIT2_WITNESS_TYPE, setUpSig
         );
+    }
+
+    // revert if invalid
+    // Note: sigs must be sorted
+    function _verifySignatures(bytes[] calldata sigs, RecoverySigs calldata details, Witness calldata witness)
+        internal
+    {
+        if (witness.threshold == 0) {
+            revert InvalidThreshold();
+        }
+
+        if (sigs.length != witness.threshold) {
+            revert NotEnoughSignatures();
+        }
+
+        address lastOwner = address(0);
+        address currentOwner;
+        bytes32 hash = details.hash();
+
+        // TODO: EIP-1271?
+        for (uint256 i = 0; i < sigs.length; ++i) {
+            currentOwner = ECDSA.recover(hash, sigs[i]);
+
+            if (currentOwner <= lastOwner) {
+                revert NotSorted();
+            }
+
+            bool isSigner;
+            for (uint256 j = 0; j < witness.signers.length; j++) {
+                if (witness.signers[j] == currentOwner) {
+                    isSigner = true;
+                    break;
+                }
+            }
+
+            if (!isSigner) {
+                revert InvalidSigner();
+            }
+
+            lastOwner = currentOwner;
+        }
     }
 }
---
+++ src/RecoverySigsLib.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.16;
+
+import {ISignatureTransfer} from "permit2/src/interfaces/ISignatureTransfer.sol";
+
+struct RecoverySigs {
+    address newAddress;
+    ISignatureTransfer.SignatureTransferDetails[] transferDetails;
+}
+
+library RecoverySigsLib {
+    bytes internal constant SIGNATURE_TRANSFER_DETAILS_TYPE =
+        abi.encodePacked("SignatureTransferDetails(", "address to,", "uint256 requestedAmount)");
+
+    bytes32 internal constant SIGNATURE_TRANSFER_DETAILS_TYPE_HASH = keccak256(SIGNATURE_TRANSFER_DETAILS_TYPE);
+
+    bytes internal constant RECOVERY_SIGS_TYPE = abi.encodePacked(
+        "RecoverySigs(", "address newAddress,", "SignatureTransferDetails[] details)", SIGNATURE_TRANSFER_DETAILS_TYPE
+    );
+
+    bytes32 internal constant RECOVERY_SIGS_TYPE_HASH = keccak256(RECOVERY_SIGS_TYPE);
+
+    function hash(ISignatureTransfer.SignatureTransferDetails calldata details) internal pure returns (bytes32) {
+        return keccak256(abi.encode(SIGNATURE_TRANSFER_DETAILS_TYPE_HASH, details.to, details.requestedAmount));
+    }
+
+    function hash(ISignatureTransfer.SignatureTransferDetails[] calldata details) internal pure returns (bytes32) {
+        bytes32[] memory hashes = new bytes32[](details.length);
+        for (uint256 i = 0; i < details.length; i++) {
+            hashes[i] = hash(details[i]);
+        }
+        return keccak256(abi.encodePacked(hashes));
+    }
+
+    /// @notice hash the given witness
+    function hash(RecoverySigs calldata data) internal pure returns (bytes32) {
+        return keccak256(abi.encode(RECOVERY_SIGS_TYPE_HASH, data.newAddress, hash(data.transferDetails)));
+    }
+}
