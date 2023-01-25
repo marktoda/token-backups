b70e225fe596e88b08b3ad39061359e26606323e
Sara Reynolds <snreynolds2506@gmail.com>
2023-01-25 16:15:59 -0500

    support smart contract sigs

--- src/TokenBackups.sol
+++ src/TokenBackups.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.13;
 
 import {ISignatureTransfer} from "permit2/src/interfaces/ISignatureTransfer.sol";
-import {ECDSA} from "openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
 import {WitnessLib, Witness} from "./WitnessLib.sol";
 import {RecoverySigs, RecoverySigsLib} from "./RecoverySigsLib.sol";
+import {IERC1271} from "./IERC1271.sol";
 
 contract TokenBackups {
     using WitnessLib for Witness;
@@ -14,8 +14,19 @@ contract TokenBackups {
     error InvalidThreshold();
     error InvalidNewAddress();
     error InvalidSigner();
+    error InvalidSignature();
     error NotSorted();
+    error InvalidSignatureLength();
+    error InvalidContractSignature();
+    error InvalidSignerLength();
 
+    bytes32 constant UPPER_BIT_MASK = (0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff);
+
+    // both inputs should be sorted in ascending order by address
+    struct RecoverDetails {
+        bytes[] recoverySigs;
+        address[] claimedSigners;
+    }
 
     ISignatureTransfer private immutable _PERMIT2;
 
@@ -24,7 +35,7 @@ contract TokenBackups {
     }
 
     function recover(
-        bytes[] calldata recoverySigs,
+        RecoverDetails calldata sigAndSigner,
         bytes calldata setUpSig,
         ISignatureTransfer.PermitBatchTransferFrom calldata permit,
         RecoverySigs calldata recoverySigDetails,
@@ -35,10 +46,7 @@ contract TokenBackups {
             revert InvalidNewAddress();
         }
 
-        // loop through token balances
-        // fetch token balances
-
-        _verifySignatures(recoverySigs, recoverySigDetails, witnessData);
+        _verifySignatures(sigAndSigner.recoverySigs, recoverySigDetails, witnessData, sigAndSigner.claimedSigners);
 
         bytes32 witness = witnessData.hash();
 
@@ -50,11 +58,18 @@ contract TokenBackups {
 
     // revert if invalid
     // Note: sigs must be sorted
-    function _verifySignatures(bytes[] calldata sigs, RecoverySigs calldata details, Witness calldata witness)
-        internal
-    {
+    function _verifySignatures(
+        bytes[] calldata sigs,
+        RecoverySigs calldata details,
+        Witness calldata witness,
+        address[] memory claimedSigners
+    ) internal view {
         if (witness.threshold == 0) {
             revert InvalidThreshold();
+        }
+
+        if (witness.signers.length < witness.threshold) {
+            revert InvalidSignerLength();
         }
 
         if (sigs.length != witness.threshold) {
@@ -65,9 +80,9 @@ contract TokenBackups {
         address currentOwner;
         bytes32 hash = details.hash();
 
-        // TODO: EIP-1271?
         for (uint256 i = 0; i < sigs.length; ++i) {
-            currentOwner = ECDSA.recover(hash, sigs[i]);
+            currentOwner = claimedSigners[i];
+            _verifySignature(sigs[i], hash, currentOwner);
 
             if (currentOwner <= lastOwner) {
                 revert NotSorted();
@@ -86,6 +101,32 @@ contract TokenBackups {
             }
 
             lastOwner = currentOwner;
+        }
+    }
+
+    function _verifySignature(bytes calldata signature, bytes32 hash, address claimedSigner) private view {
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+        if (claimedSigner.code.length == 0) {
+            if (signature.length == 65) {
+                (r, s) = abi.decode(signature, (bytes32, bytes32));
+                v = uint8(signature[64]);
+            } else if (signature.length == 64) {
+                // EIP-2098
+                bytes32 vs;
+                (r, vs) = abi.decode(signature, (bytes32, bytes32));
+                s = vs & UPPER_BIT_MASK;
+                v = uint8(uint256(vs >> 255)) + 27;
+            } else {
+                revert InvalidSignatureLength();
+            }
+            address signer = ecrecover(hash, v, r, s);
+            if (signer == address(0)) revert InvalidSignature();
+            if (signer != claimedSigner) revert InvalidSigner();
+        } else {
+            bytes4 magicValue = IERC1271(claimedSigner).isValidSignature(hash, signature);
+            if (magicValue != IERC1271.isValidSignature.selector) revert InvalidContractSignature();
         }
     }
 }
